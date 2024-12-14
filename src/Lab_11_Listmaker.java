c2dec6bd750172cb33044c6cde46877d0981c890
hunter <westerhr@mail.uc.edu>
2024-12-13 22:09:22 -0500

    Initial commit

---
+++ src/Lab_11_Listmaker.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Lab_11_Listmaker {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
